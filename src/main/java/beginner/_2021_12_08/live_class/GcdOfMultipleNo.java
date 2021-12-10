a0a54c01b64de202c9d56f342c38d6cbfa1bbc11
suhas1294 <suhasr94@gmail.com>
2021-12-10 18:55:36 +0530

    updates latest class assignment and homeworks

---
+++ src/main/java/beginner/_2021_12_08/live_class/GcdOfMultipleNo.java
@@ -0,0 +1,17 @@
+package beginner._2021_12_08.live_class;
+
+import beginner._2021_12_08.assignments.EuclidsGcdAlgoRecursion;
+
+import java.util.Scanner;
+
+public class GcdOfMultipleNo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int T = scanner.nextInt();
+        int gcd = 0;
+        for (int i = 0; i < T; i++) {
+            gcd = EuclidsGcdAlgoRecursion.gcdEuclidRecursionAlgo(gcd, scanner.nextInt());
+        }
+        System.out.println(gcd);
+    }
+}
---
+++ src/main/java/beginner/_2021_12_08/live_class/SumOfDivisorsOptimized.java
@@ -0,0 +1,21 @@
+package beginner._2021_12_08.live_class;
+
+import java.util.Scanner;
+
+public class SumOfDivisorsOptimized {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int sum = 0;
+        for (int i = 2 ; i <= Math.sqrt(N); i++) {
+            if ( N % i == 0){
+                if ( i * i == N){
+                    sum += i;
+                }else{
+                    sum += i + N/i; // Main part of code that optimizes the algorithm
+                }
+            }
+        }
+        System.out.println(sum);
+    }
+}
