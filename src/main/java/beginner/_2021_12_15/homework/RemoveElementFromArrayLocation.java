959883d407408d7789ad1f8818dbd37a7232f0a9
suhas1294 <suhasr94@gmail.com>
2021-12-16 23:23:37 +0530

    updates class notes and assignments

---
+++ src/main/java/beginner/_2021_12_15/homework/RemoveElementFromArrayLocation.java
@@ -0,0 +1,23 @@
+package beginner._2021_12_15.homework;
+
+import java.util.Scanner;
+
+public class RemoveElementFromArrayLocation {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        int X = scanner.nextInt(); // position where number needs to be removed, X > 1
+
+        for (int i = X-1; i < arr.length-1; i++) {
+            arr[i] = arr[i+1];
+        }
+        // print array
+        for (int i = 0; i < arr.length-1; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
