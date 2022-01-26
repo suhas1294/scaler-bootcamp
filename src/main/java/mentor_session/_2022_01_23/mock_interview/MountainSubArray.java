d153e51b4c95eb646de4ee88780f5f0aec0d8e46
Suhas Ramesh <suhasr94@gmail.com>
2022-01-26 15:52:20 +0530

    Update MountainSubArray.java

--- src/main/java/mentor_session/_2022_01_23/mock_interview/MountainSubArray.java
+++ src/main/java/mentor_session/_2022_01_23/mock_interview/MountainSubArray.java
@@ -1,45 +1,37 @@
 package mentor_session._2022_01_23.mock_interview;
 
 public class MountainSubArray {
-    public static void solve(int[] arr){
-        int start = 0;
-        int end = 0;
-
-
-        while(end<arr.length){
-            if(arr[end]<arr[end+1]){
-
-                while(arr[end]<arr[end+1]){
-                    end++;
+    public static int longestMountain(int[] arr){
+        int ws = 0;
+        int we = 0;
+        int maxMountainLength = 0;
+        while(ws < arr.length && we < arr.length){
+            if (we+1 < arr.length && arr[we+1] > arr[we]) {
+                while (we+1 < arr.length && arr[we + 1] > arr[we]){ // climb up
+                    we++;
                 }
-                if(arr[end]>arr[end+1]){
-
-                    while(arr[end]>arr[end+1]){
-                        end++;
-
-                        max = Math.max(max, end-start+1);
-                        start=end;
+                if (we+1 < arr.length && arr[we + 1] < arr[we]){
+                    while (we+1 < arr.length && arr[we + 1] < arr[we]){ // climb down
+                        we++;
                     }
+                    maxMountainLength = Math.max(maxMountainLength, we-ws+1);
+                    ws = we;
                 }
-            }
-        }
-
-        boolean ended = false;
-        int maxLen = 0;
-        while(end < arr.length){
-            if (arr[end] > arr[start]){
-                ended = false;
-                end++;
-                continue;
             }else{
-                end++;
-                ended = true;
-                start = end-1;
-                curLen = end - start + 1;
-                maxLen = Math.max(maxLen, curLen);
+                ws++;
+                we = ws;
             }
         }
-        return maxLen;
+        return maxMountainLength;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(longestMountain(new int[]{1, 3, 1, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5}));
+        System.out.println(longestMountain(new int[]{2,1,4,7,3,2,5})); // 5
+        System.out.println(longestMountain(new int[]{2,3})); // 0
+        System.out.println(longestMountain(new int[]{0,2,0,2,1,2,3,4,4,1})); // 3
+        System.out.println(longestMountain(new int[]{875,884,239,731,723,685})); // 4
+        System.out.println(longestMountain(new int[]{0,1,0,0,1,0,0,1,1,0,0,0,1,1,0,1,0,1,0,1,0,0,0,1,0,0,1,1,0,1,1,1,1,1,0,0,1,0,1,1,0,0,0,0,0,0,0,0,1,1,0,0,1,1,1,1,0,0,0,1,0,0,1,1,0,0,0,1,0,0,1,1,0,0,0,0,1,0,0,1,1,1,1,1,1,1,0,1,1,0,1,1,1,0,0,0,1,0,1,1})); // 3
     }
 }
 
